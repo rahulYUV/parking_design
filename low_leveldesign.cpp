1e260314a8b7de9bef7e72d284993a4a4e23a0e3
rahul yuv <138332085+rahulYUV@users.noreply.github.com>
2024-01-30 22:32:00 +0000

    seccond_commit

--- low_leveldesign.cpp
+++ low_leveldesign.cpp
@@ -1,82 +1,78 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>resume</title>
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <link href="bootstrap.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
-        .one{ font-weight:bold;
-            font-family:Fira Code;
-            text-align: center;
+#include <iostream>
+#include <vector>
+
+class Car {
+public:
+    std::string licensePlate;
+
+    Car(const std::string& plate) : licensePlate(plate) {}
+};
+
+class ParkingSpace {
+public:
+    bool isOccupied;
+    Car* parkedCar;
+
+    ParkingSpace() : isOccupied(false), parkedCar(nullptr) {}
+};
+
+class ParkingLot {
+private:
+    std::vector<ParkingSpace> spaces;
+
+public:
+    ParkingLot(int capacity) {
+        spaces.resize(capacity);
+    }
+
+    bool parkCar(Car* car) {
+        for (auto& space : spaces) {
+            if (!space.isOccupied) {
+                space.isOccupied = true;
+                space.parkedCar = car;
+                return true;
+            }
         }
-    </style>
-</head>
-<body>
-<div class="container" >
-    <div class="list-group one">
-        <div class="list-group-item list-group-item-dark ">Shradha Khapra</div>
-    </div>
-    <br><br>
-    <div class="list-group">
-        <div class=" list-group-item list-group-item-dark">
-            <i class="fa fa-briefcase"></i> CAREER OBJECTIVE</div>
-        <div class="list-group-item">I wish to create an impact in the field of computer sciences via developing a set of technology-oriented skills.</div>
-    </div>
-    <br><br>
-    <div class="list-group">
-        <div class="list-group-item list-group-item-dark"><i class="fa fa-mortar-board"></i> EDUCATION</div>
-        <div class="list-group-item list-group-item-secondary">Jain Bharati Mrigavati Vidyalaya</div>
-        <div class="list-group-item"><i class="fa fa-arrows"></i> Secured CGPA 10 in 10th grade</div>
-        <div class="list-group-item"><i class="fa fa-arrows"></i> Secured 94.4% in 12th grade.</div>
-    </div>
-    <div class="list-group">
-        <div class="list-group-item list-group-item-secondary">Netaji Subhas institute of Technology</div>
-        <div class="list-group-item"><i class="fa fa-arrows"></i> Pursuing Bachelor of Engineering(BE)</div>
-        <div class="list-group-item"><i class="fa fa-arrows"></i> Computer Engineering(Batch of 2021)</div>
-    </div>
-    <br><br>
-    <div class="list-group">
-        <div class="list-group-item list-group-item-dark"><i class="fa fa-laptop"></i> SKILLS</div>
-        <div class="list-group-item"><i class="fa fa-arrows"></i> Coding : C,C++ </div>
-        <div class="list-group-item"><i class="fa fa-arrows"></i> Web-Development : HTML,CSS,JS</div>
+        return false; // Parking lot is full
+    }
 
-    </div>
+    void removeCar(Car* car) {
+        for (auto& space : spaces) {
+            if (space.isOccupied && space.parkedCar == car) {
+                space.isOccupied = false;
+                space.parkedCar = nullptr;
+                return;
+            }
+        }
+    }
 
-    <br><br>
-    <div class="list-group">
-        <div class="list-group-item list-group-item-dark"><i class="fa fa-sitemap"></i> WORK</div>
-    </div>
-    <div class="row">
-        <div class="card col-sm-12 col-md-6">
-            <div class=" card-body">
-                <h5 class="card-title">TEAMC & HACKEAM'18 </h5>
-                <p class="card-text">Working as a volunteer in the organisation of HACKEAM'18 i.e a hackathon to solve problems centred on Extensible and Adaptable methods,to be held in NSIT in January 2018.</p>
-            </div> </div>
-        <div class="card col-sm-12 col-md-6">
-            <div class=" card-body">
-                <h5 class="card-title">DELHI SECTION CONGRESS 2017 </h5>
-                <p class="card-text">Participated in a play organised as a part of Delhi Section Congress 2017 organised by IEEE Delhi Section held at NSIT in October 2017.</p>
-            </div> </div>
-    </div>
-    <br><br>
-    <div class="list-group">
-        <div class="list-group-item list-group-item-dark"><i class="fa fa-trophy"></i> MAJOR ACHIEVEMENTS</div>
-        <div class="list-group-item "><i class="fa fa-arrows"></i> Secured 2nd position at State level debating(2015-16)</div>
-        <div class="list-group-item "><i class="fa fa-arrows"></i> Secured 23rd State and 127 International rank in National Science Olympiad(2016-17)</div>
-        <div class="list-group-item "><i class="fa fa-arrows"></i> Participant in CBSE National Volleyball Championship(2015-16)</div>
-        <div class="list-group-item "><i class="fa fa-arrows"></i> Secured positions in zonal & inter-zonal level volleyball & throwball</div>
-        <div class="list-group-item "><i class="fa fa-arrows"></i> Secured positions in zonal & inter-zonal level declamations,debates and poem recitations</div>
-    </div>
-    <br><br>
-    <div class="list-group">
-        <div class="list-group-item list-group-item-dark">CONTACT</div>
-        <div class="list-group-item"><i class="fa fa-envelope"></i> shradhakhapra1899@gmail.com</div>
-        <div class="list-group-item"><i class="fa fa-phone"></i> 8920432858</div>
-        <div class="list-group-item"><i class="fa fa-github"></i><a href="https://github.com/shradha-khapra"> Github</a> </div>
-    </div>
+    void printParkingStatus() {
+        for (int i = 0; i < spaces.size(); ++i) {
+            if (spaces[i].isOccupied) {
+                std::cout << "Space " << i + 1 << ": Occupied by Car with License Plate - " << spaces[i].parkedCar->licensePlate << std::endl;
+            } else {
+                std::cout << "Space " << i + 1 << ": Empty" << std::endl;
+            }
+        }
+    }
+};
 
-</div>
-</body>
-</html>
+int main() {
+    ParkingLot parkingLot(10);
+
+    Car car1("ABC123");
+    Car car2("XYZ789");
+     Car car6("XYZ789");
+
+    parkingLot.parkCar(&car1);
+    parkingLot.parkCar(&car2);
+
+    parkingLot.printParkingStatus();
+
+    parkingLot.removeCar(&car1);
+
+    std::cout << "\nAfter removing car with license plate ABC123:\n";
+    parkingLot.printParkingStatus();
+
+    return 0;
+}
